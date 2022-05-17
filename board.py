77c0fe86ba71b4e939b188a93811dc8c15812a16
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2022-05-17 22:13:17 +0200

    cleanup and comments

--- board.py
+++ board.py
@@ -19,6 +19,9 @@ class Board:
 
 
     def next_empty(self, i) -> Cell or None:
+        """
+        Starting from cell i return the next empty cell, moving in the reading direction
+        """
         while self.cells[i].value != 0:
             i += 1
             if i == self.w*self.h:
@@ -28,12 +31,20 @@ class Board:
 
 
     def group_valid(self, group_id: int) -> bool:
+        """
+        Checks if the group with a given group_id violates the rules, which means it returns true
+        if any number appears more than once in the group (excluding 0)
+        """
         cell_values = [c.value for g, c in zip(self.grouping, self.cells) if g == group_id]
         return sum(cell_values) == sum(set(cell_values))
         
     
     # TODO: Not the neatest way of checking
     def neighbours_valid(self, cell_id: int) -> bool:
+        """
+        Returns whether a cell with a given cell_id violates the neighbour rules, i.e. returns
+        true if has same value as any of the neighbours including dialgonal
+        """
         x, y = self.coords_from_id(cell_id)
         
         # Dont bother with checking empty cells
@@ -58,6 +69,7 @@ class Board:
         
         return True
         
+
     def __repr__(self) -> str:
         lines = ""
         for y in range(self.h):
@@ -67,15 +79,24 @@ class Board:
             lines += f"{line}\n"
         return lines
     
+
     def path_to(self, cell_id: int) -> str:
+        """
+        Get the series of numbers leading to a cell at a given position. Used to keep track of 
+        which paths have already been explored
+        """
         return "".join([str(self.cells[x].value) for x in range(cell_id+1)])
 
-    def num_cells_in_group(self, id: int) -> int:
-        return self.grouping.count(id)
 
+    def num_cells_in_group(self, id: int) -> int:
+        """
+        Returns the number of cells that belong to a group with a given id
+        """
+        return self.grouping.count(id)
 
     def set_cell_value(self, cell_id: int, new_val: int) -> None:
         self.cells[cell_id].value = new_val
+
 
     def id_from_coords(self, x: int, y:int) -> int:
         return y*self.w + x
@@ -85,6 +106,9 @@ class Board:
 
 
     def as_list(self) -> list:
+        """
+        Return the cell values as a 2-dimensional list 
+        """
         vals = [x.value for x in self.cells]
         vals_2d = []
         for i in range(self.w, self.h*self.w + self.w, self.w):
--- solver.py
+++ solver.py
@@ -1,16 +1,23 @@
 from board import Board
-import util
 
 class Solver:
 
     def __init__(self, initial_board: Board) -> None:
-        self.board = initial_board
-        self.boards = []
-        self.values_tried = []
         self.recursions_deep = 0
+        self.board = initial_board
+        self.values_tried = []
+        
+        # Only used for animating the solution later
+        self.boards = []
+    
 
-
+    # TODO: Add clarification to user when puzzle has no solution
     def solve(self, b: Board, current: int = 0) -> bool:
+        """
+        Recursive backtracking function call to solve a suguru puzzle. Moves in the reading direction 
+        and backtracks one step whenever it reaches an invalid board state, continues untill the solution
+        is found or no solution can be found.
+        """
         self.recursions_deep += 1
         next = b.next_empty(current)
 
@@ -18,7 +25,10 @@ class Solver:
             print(f"Solution found going {self.recursions_deep} recursions deep")
             return True
 
+        # Loop through every possible value the cell can have, i.e. the number of cells in its group
         for val in range(1, b.num_cells_in_group(next.group_id)+1):
+
+            # Skip if we have already explored this path
             if b.path_to(next.cell_id)+str(val) in self.values_tried:
                 continue
             
@@ -26,10 +36,12 @@ class Solver:
             self.boards.append(b.as_list())
             self.board = b
 
+            # If the state is valid do recursion
             if self.board_valid(b):
                 if self.solve(b, current+1):
                     return True
 
+            # Backtrack
             self.values_tried.append(b.path_to(next.cell_id)+str(val))
             b.set_cell_value(next.cell_id, 0)
             self.boards.append(b.as_list())
@@ -38,6 +50,10 @@ class Solver:
 
 
     def board_valid(self, b: Board) -> bool:
+        """
+        Calls group_valid and neighbours_valid functions on the board to determine wether all cells and
+        groups are in valid configurations, if this is the case the board is legal and return true
+        """
         groups_valid = all([b.group_valid(id) for id in range(b.num_groups)])
         neighbours_valid = all([b.neighbours_valid(id) for id in range(b.w*b.h)])
 
