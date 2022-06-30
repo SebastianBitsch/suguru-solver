77b3387e399c9c7eb6c8bba3d1f13b4929f422b1
Sebastian Bitsch <sebastianbitsch@protonmail.com>
2022-06-30 22:52:58 +0200

    nicer plotting

--- main.py
+++ main.py
@@ -19,7 +19,34 @@ grouping = sample_board.grouping
 
 # Set up plot
 fig = plt.figure(figsize=(7,7))
-im = plt.imshow(grouping, interpolation='none', aspect='auto',cmap='tab20c')
+# im = plt.imshow(grouping, interpolation='none', aspect='auto',cmap='tab20c')
+
+
+from MarchingSquares import MarchingSquares
+
+def draw_board(board_size:tuple):
+    pass
+    # y = [[1,1], [2,1], [2,2], [1,2], [0.5,1.5]]
+
+    # p = plt.Polygon(y, facecolor = 'k')
+
+    # fig, ax = plt.subplots()
+
+    # ax.add_patch(p)
+    # ax.set_xlim([0,3])
+    # ax.set_ylim([0,3])
+    # plt.show()
+    # plt.figure(figsize=(7,7))
+    # plt.axes(xlim=(0, board_size[0]), ylim=(0, board_size[1]))
+    #plt.title(options.title, loc='left')
+    #plt.title("Bowyer-Watson Triangulation", loc='center')
+    # plt.xticks([])
+    # plt.yticks([])
+
+    # Plot bounds
+    # b = plt.Rectangle((0,0),2,1, color='black', fill=False, zorder=0)
+    # plt.gca().add_patch(b)
+    # plt.show()
 
 
 def animate_text(i):
@@ -52,17 +79,36 @@ def animate_solution():
     plt.show()
 
 
+import matplotlib.pyplot as plt
 if __name__ == "__main__":
-    b = Board(initial_board, grouping)
-    s = Solver(b)
 
-    s.solve(b)
+    # grid = [
+    #     [1,1,1,1],
+    #     [1,2,2,1],
+    #     [1,2,2,1],
+    #     [1,1,1,1]
+    # ]    
+    # ms = MarchingSquares(grid, lower_threshold=2)
+    # ms.plot_edges(plot_grid=True)
+    plt.xlim((0,8))
+    plt.ylim((0,7))
+    
+    for i in range(12):
+        ms = MarchingSquares(grouping, lower_threshold=i, upper_threshold=i)
+        ms.plot_edges()
+    plt.show()
 
-    # Solved board
-    print(s.board)
+    #draw_board((8,7))
+    # b = Board(initial_board, grouping)
+    # s = Solver(b)
 
-    # Animate solution
-    snapshots = s.boards
-    configure_plot(s.board.w, s.board.h)
-    animate_solution()
+    # s.solve(b)
+
+    # # Solved board
+    # print(s.board)
+
+    # # Animate solution
+    # snapshots = s.boards
+    # configure_plot(s.board.w, s.board.h)
+    # animate_solution()
 
